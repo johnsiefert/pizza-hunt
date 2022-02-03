5cdcac393aee5028dae27d99ebce72da0938468b
johnsiefert <johnsief12@gmail.com>
2022-02-03 18:21:18 -0500

    added indexedDB

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -22,6 +22,33 @@ const commentController = {
       .catch(err => res.json(err));
   },
 
+  addReply({ params, body }, res) {
+    Comment.findOneAndUpdate(
+      { _id: params.commentId },
+      { $push: { replies: body } },
+      { new: true }
+    )
+      .then(dbPizzaData => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: 'No pizza found with this id!' });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch(err => res.json(err));
+  },
+
+// remove reply Here, we're using the MongoDB $pull operator to remove the specific reply from the replies array where the replyId matches the value of params.replyId passed in from the route
+removeReply({ params }, res) {
+  Comment.findOneAndUpdate(
+    { _id: params.commentId },
+    { $pull: { replies: { replyId: params.replyId } } },
+    { new: true }
+  )
+    .then(dbPizzaData => res.json(dbPizzaData))
+    .catch(err => res.json(err));
+},
+
    //remove comment
    removeComment({ params }, res) {
     Comment.findOneAndDelete({ _id: params.commentId })
